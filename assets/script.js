c9e25df5296e4b1d92115c762a29848d789b8736
Michael Hobert <michaelhobertjr315@gmail.com>
2023-05-18 19:02:18 -0400

    fixed all issues

--- assets/script.js
+++ assets/script.js
@@ -1,70 +1,70 @@
-var checkbox = document.querySelector("#checkbox")
-var genpas = document.querySelector("#genpas")
-var passwordgenbutton = document.querySelector("passwordgenbutton")
-var generate = document.getElementById("generate")
-var p2 = document.getElementById("password")
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
 
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-var Password = '';
+  passwordText.value = password;
+}
 
-var charsLowerCase = "abcdefghijklmnopqrstuvwxyz";
-var charsUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var charsBothCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-var charsLowerNumCase = "abcdefghijklmnopqrtuvwxyz0123456789";
-var charsUpperNumCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-var charsLowerUpperNumCase = "abcdefghijklmnopqrtuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-var charsNum = "0123456789";
-var charsSpecial = "!@#$%^&*";
-var charsNumSpecial = "0123456789!@#$%^&*";
-var charsLowerCaseSpecial = "abcdefghijklmnopqrstuvwxyz!@#$%^&*";
-var charsUpperCaseSpecial = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*";
-var charsLowerCaseNumSpecial = "0123456789!@#$%^&*abcdefghijklmnopqrstuvwxyz";
-var charsAllCase = "0123456789!@#$%^&*ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-var charsUpperCaseNumSpecial = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*0123456789";
-var password = "";
+// Generate the password based on user-selected criteria
+function generatePassword() {
+  // Prompt for password criteria
+  var length = parseInt(prompt("Enter the desired length of your password (between 8 and 128 characters):"));
 
-// these are the prompts for input
-
-var criteria = window.prompt("Please enter your password criteria: lowercase, uppercase, bothcase, allcase, numcase, specialcase, lowernumbercase, uppernumbercase, ");
-
-var length = window.prompt("What is the length?");
-
-// generatePassword function  
-function generatePassword(criteria, length) {
-    // if statement to determine criteria
-    if (criteria.toLowerCase() === "lowercase") {
-      charset = charsLowerCase;
-    } else if (criteria.toLowerCase() === "uppercase") {
-      charset = charsUpperCase;
-    } else if (criteria.toLowerCase() === "bothcase") {
-      charset = charsBothCase;
-    } else if (criteria.toLowerCase() === "allcase") {
-      charset = charsAllCase;
-    } else if (criteria.toLowerCase() === "numcase") {
-      charset = charsNum;
-    } else if (criteria.toLowerCase() === "specialcase") {
-      charset = charsSpecial;
-    } else if (criteria.toLowerCase() === "lowernumbercase") {
-      charset = charsLowerNumCase;
-    } else if (criteria.toLowerCase() === "uppernumbercase") {
-      charset = charsUpperNumCase;
-    } else {
-      console.error();
-    }
-  
-    // if statement to determine whether length will suffice
-    if (length >= 8 && length <= 64) {
-      // actual function
-      for (var i = 0; i < length; i++) {
-        password += charset.charAt(Math.floor(Math.random() * charset.length));
-      }
-      return password;
-    }
+  // Validate the input for password length
+  if (isNaN(length) || length < 8 || length > 128) {
+    alert("Invalid password length. Please enter a number between 8 and 128.");
+    return "";
   }
 
+  var includeLowercase = confirm("Include lowercase characters?");
+  var includeUppercase = confirm("Include uppercase characters?");
+  var includeNumeric = confirm("Include numeric characters?");
+  var includeSpecial = confirm("Include special characters?");
 
+  // Validate that at least one character type is selected
+  if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
+    alert("Please select at least one character type.");
+    return "";
+  }
 
-generate.addEventListener("click", function () {
-    p2.innerText = generatePassword(criteria, length);
-  });
+  // Define character sets based on selected criteria
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericChars = "0123456789";
+  var specialChars = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
 
+  var availableChars = "";
+
+  if (includeLowercase) {
+    availableChars += lowercaseChars;
+  }
+
+  if (includeUppercase) {
+    availableChars += uppercaseChars;
+  }
+
+  if (includeNumeric) {
+    availableChars += numericChars;
+  }
+
+  if (includeSpecial) {
+    availableChars += specialChars;
+  }
+
+  var password = "";
+
+  // Generate the password by randomly selecting characters from the available character set
+  for (var i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * availableChars.length);
+    password += availableChars.charAt(randomIndex);
+  }
+
+  return password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
